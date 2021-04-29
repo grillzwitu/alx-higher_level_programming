4d3f7b4681f289a13630cd7a221650695258338b
Tare Appah <grillzwitu@gmail.com>
2021-04-29 21:22:10 +0100

    Update 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,7 +3,6 @@ from calculator_1 import add, sub, mul, div
 from sys import argv, exit
 
 if __name__ == "__main__":
-
     #Throw an error if arguements are not 3
     if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
@@ -13,17 +12,17 @@ if __name__ == "__main__":
         b = int(argv[3])
     #If argv[2](operator) is +
         if argv[2] == '+':
-            print("{} + {} = {:d}".format(a, b, add(a, b)))
+            print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
     #If argv[2](operator) is -
         elif argv[2] == '-':
-            print("{} - {} = {:d}".format(a, b, sub(a, b)))
+            print('{:d} - {:d} = {:d}'.format(a, b, sub(a, b)))
     #If argv[2](operator) is *
         elif argv[2] == '*':
-            print("{} * {} = {:d}".format(a, b, mul(a, b)))
+            print('{:d} * {:d} = {:d}'.format(a, b, mul(a, b)))
     #If argv[2](operator) is /
         elif argv[2] == '/':
-            print("{} / {} = {:d}".format(a, b, div(a, b)))
+            print('{:d} / {:d} = {:d}'.format(a, b, div(a, b)))
     #If operator is not +, -, * or /
         else:
-            print("Unknown operator. Available operators: +, -, * and /")
+            print('Unknown operator. Available operators: +, -, * and /')
             exit(1)
