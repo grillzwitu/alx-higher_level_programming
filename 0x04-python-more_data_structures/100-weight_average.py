74b849c58ae05806747c17ee7035a2469650877d
grillzwitu <grillzwitu@gmail.com>
2021-05-06 18:52:27 +0000

    corrected

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -8,4 +8,4 @@ def weight_average(my_list=[]):
     for i in my_list:
         sumWeightedScores += i[0] * i[1]
         sumTotalWeights += i[1]
-    return  sumWeightedScores/sumTotalWeights
+    return sumWeightedScores/sumTotalWeights
