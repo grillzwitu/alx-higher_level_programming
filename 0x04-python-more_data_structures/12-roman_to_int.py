36762f778ec782da1c9517627fae004f9a01b395
Tare Appah <grillzwitu@gmail.com>
2021-05-06 14:04:27 +0100

    Update 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,7 +4,7 @@ def roman_to_int(roman_string):
         "I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "DM": 900,
         "M": 1000, "IV": 4, "IX": 9, "XL": 40, "XC": 90, "CD": 400
     }
-    if not isinstance(roman_string, str) or roman_string is None:
+    if isinstance(roman_string, str) is false or roman_string is None:
         return 0
     l = len(roman_string)
     c = 0
