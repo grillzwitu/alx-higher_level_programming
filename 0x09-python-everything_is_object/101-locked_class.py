7d27d22231144bce1996d15ac6c78c983834bf5c
grillzwitu <grillzwitu@gmail.com>
2021-05-25 19:54:58 +0000

    locked class

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
+"""Defines a class module"""
+
+
 class LockedClass:
-    """A locked class that only lets the user create the instance
+    """A class that only lets the user create the instance
     attribute 'first_name'"""
     __slots__ = ['first_name']
