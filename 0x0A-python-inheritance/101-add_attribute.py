8cecc36c8786cba11abfd0a4d179861ffee64e4a
grillzwitu <grillzwitu@gmail.com>
2021-05-31 19:20:19 +0000

    added doc string

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+"""
+   Implements a function that checks for and adds an attribute
+"""
+
+
 def add_attribute(cls, name, first):
+    """Checks for and Adds Attribute"""
     if not hasattr(cls, name):
         raise TypeError("can't add new attribute")
     cls.name = first
