43287033f6c836f2bf3349c68ee7f52e5ed8daef
grillzwitu <grillzwitu@gmail.com>
2021-06-02 10:00:16 +0000

    101-stat

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -10,30 +10,39 @@ def print_status():
         Printing the status of the request
     """
     counter = 0
-    size = 0
     file_size = 0
     status_codes = {"200": 0, "301": 0, "400": 0, "401": 0,
                     "403": 0, "404": 0, "405": 0, "500": 0}
 
-    for l in sys.stdin:
-        line = l.split()
-        try:
-            size += int(line[-1])
-            code = line[-2]
-            status_codes[code] += 1
-        except:
-            continue
-        if counter == 9:
-            print("File size: {}".format(size))
-            for key, val in sorted(status_codes.items()):
-                if (val != 0):
-                    print("{}: {}".format(key, val))
-            counter = 0
-        counter += 1
-    if counter < 9:
-        print("File size: {}".format(size))
+    try:
+        for l in sys.stdin:
+            line = l.split()
+            if len(line) >= 2:
+                c = counter
+                if line[-2] in status_codes:
+                    status_codes[line[-2]] += 1
+                    counter += 1
+                try:
+                    file_size += int(line[-1])
+                    if c == counter:
+                        counter += 1
+                except:
+                    if c == counter:
+                        continue
+            if counter % 10 == 0:
+                print("File size: {}".format(file_size))
+                for key, val in sorted(status_codes.items()):
+                    if val:
+                        print("{}: {}".format(key, val))
+        print("File size: {}".format(file_size))
         for key, val in sorted(status_codes.items()):
-            if (val != 0):
+            if val:
+                print("{}: {}".format(key, val))
+
+    except KeyboardInterrupt:
+        print("File size: {}".format(file_size))
+        for key, val in sorted(status_codes.items()):
+            if val:
                 print("{}: {}".format(key, val))
 
 
