334036d418738a3fec8c86ca1b9d1a92bf79ab5b
grillzwitu <grillzwitu@gmail.com>
2021-08-02 15:17:54 +0000

    refactored just checking

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,8 +2,13 @@
 'use strict';
 let secMax = 0;
 const args = process.argv.slice(2);
+let newArr = [];
 if (args.length > 1) {
   args.sort();
-  secMax = args[args.length - 2];
+  for (let arg = 0; arg < args.length; arg++) {
+    let numArg = Number(arg);
+    newArr.push(numArg);
+  }
+  secMax = newArr[newArr.length - 2];
 }
 console.log(secMax);
--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 'use strict';
 const args = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (const arg in args) {
+for (let arg = 0; arg < args.length; arg++) {
   console.log(args[arg]);
 }
