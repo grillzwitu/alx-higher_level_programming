35dd6455a0e2ec4f890f777bdec881f49a33a610
grillzwitu <grillzwitu@gmail.com>
2021-08-02 15:21:43 +0000

    added ^Cpital S in javascript

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 'use strict';
-const args = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const args = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let arg = 0; arg < args.length; arg++) {
   console.log(args[arg]);
 }
