bb822a68970bd8ef79dc318e8451321333f94a39
grillzwitu <grillzwitu@gmail.com>
2021-08-19 17:39:10 +0000

    tasks 0-8

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""contains the function find_peak"""
+
+def find_peak(list_of_integers):
+    """Find highest value in a list of unsorted integers"""
+
+    int_list = list_of_integers
+
+    if int_list:
+        int_list.sort()
+        return int_list[-1]
+    else:
+        return None
