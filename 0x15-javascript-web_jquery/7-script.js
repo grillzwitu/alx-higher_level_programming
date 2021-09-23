045d0a724973a5a311af02ce0ce6d47b863a8705
grillzwitu <grillzwitu@gmail.com>
2021-09-23 06:55:01 +0000

    initial commit

---
+++ 0x15-javascript-web_jquery/7-script.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+$(document).ready(function () {
+  const url = 'https://swapi-api.hbtn.io/api/people/5/?format=json';
+  $.get(url, function (data) {
+    $('#character').text(data.name);
+  });
+});
